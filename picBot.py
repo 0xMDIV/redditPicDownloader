2fb74dffbe1b980cfcb6fc419600e609659f22ff
0xMDIV <0x.mdiv@gmail.com>
2023-02-19 00:55:29 +0100

    pushed changes to picbot

--- picBot.py
+++ picBot.py
@@ -8,7 +8,7 @@ path = os.getcwd()+'/'
 url = "https://www.reddit.com/"
 name = 0
 
-def fetch_images(submission):
+def fetch_images(submission, sub):
     url = (submission.url)
     filename = str(submission.id)
     # filename = str(name)
@@ -22,20 +22,20 @@ def fetch_images(submission):
         found = False
     
     if found == True: # if image found save it
-        if os.path.isfile(path+args['subreddit']+'/'+filename): # check if the found image already exists in the given path 
+        if os.path.isfile(path+sub+'/'+filename): # check if the found image already exists in the given path 
             print("This picture already exists")
         else:
             r = requests.get(url)
             with open(filename, 'wb') as w:
                 w.write(r.content)
 
-            shutil.move(path+filename,path+args["subreddit"]) # move the downloaded image to the folder(named after subreddit)
+            shutil.move(path+filename,path+sub) # move the downloaded image to the folder(named after subreddit)
             print(filename)
 
 def subreddit(sub, number, sort, time, reddit):
     
     # init the subreddit
-    subreddit = reddit.subreddit(args["subreddit"])
+    subreddit = reddit.subreddit(sub)
     name = 0
 
     if sort == 'top': # sort search by top with given time_filter i.e day, month etc.
@@ -44,7 +44,7 @@ def subreddit(sub, number, sort, time, reddit):
             if name == number+1:
                 break
             else:
-                fetch_images(submission)
+                fetch_images(submission, sub)
 
     if sort == "hot": # sort search by hot
         for submission in subreddit.hot(limit=number):
@@ -52,7 +52,7 @@ def subreddit(sub, number, sort, time, reddit):
             if name == number+1:
                 break
             else:
-                fetch_images(submission)
+                fetch_images(submission, sub)
 
     if sort == "best": # sort search by best
         for submission in subreddit.best(limit=number):
@@ -60,7 +60,7 @@ def subreddit(sub, number, sort, time, reddit):
             if name == number+1:
                 break
             else:
-                fetch_images(submission)
+                fetch_images(submission, sub)
 
     if sort == "new": # sort search by new
         for submission in subreddit.new(limit=number):
@@ -68,7 +68,7 @@ def subreddit(sub, number, sort, time, reddit):
             if name == number+1:
                 break
             else:
-                fetch_images(submission)
+                fetch_images(submission, sub)
         
 
 def sub_exists(sub, reddit):
@@ -88,9 +88,9 @@ def createDir(sub):
 
 # def main():
 parser = argparse.ArgumentParser()
-parser.add_argument('-sub',"--subreddit", required=True, help="Name of subreddit")
-parser.add_argument('-n',"--number", required=True, type=int, help="Number of images")
-parser.add_argument('-sort',"--sort", help="Sort by", default='hot')
+parser.add_argument('-sub',"--subreddit", required=False, help="Name of subreddit")
+parser.add_argument('-n',"--number", required=False, type=int, help="Number of images")
+parser.add_argument('-sort',"--sort", help="Sort by", default='new')
 parser.add_argument('-t',"--time_filter", help="Top of day/month/year", default='day')
 args = vars(parser.parse_args())
 
@@ -103,9 +103,13 @@ reddit = praw.Reddit(client_id = params['client_id'],
             client_secret = params['client_secret'],
             user_agent = params['user_agent'])
 
-if not sub_exists(args["subreddit"], reddit):
-    print(f"Subreddit: {args['subreddit']} not found")
-    exit
-else:
-    createDir(args['subreddit'])
-    subreddit(args['subreddit'], args['number'], args['sort'], args['time_filter'], reddit)
+f_final = open("sub_list.csv", "r")
+for line in f_final:
+    sub = line.strip()
+
+    if not sub_exists(sub, reddit):
+        print(f"Subreddit: {sub} not found")
+        exit
+    else:
+        createDir(sub)
+        subreddit(sub, 100, args['sort'], "month", reddit)
